92a2d1229e8d481d63c934115dcdc2047be1357c
Simon Kodu <siko0533@dsv.su.se>
2025-04-28 12:36:55 +0200

    Moved to trashcan

---
+++ src/main/java/com/pvt/demo/trashcan/SKEventRepository.java
@@ -0,0 +1,5 @@
+// package com.pvt.demo.trashcan;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// public interface SKEventRepository extends JpaRepository<SKEvent, Integer> {}
--- src/main/java/com/trashcan/SKEventRepository.java
+++
@@ -1,5 +0,0 @@
-package com.trashcan;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SKEventRepository extends JpaRepository<SKEvent, Integer> {}
