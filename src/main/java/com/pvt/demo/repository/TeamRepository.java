6ba600d50b44f3a4047a9997707748ba49af2692
Simon Kodu <siko0533@dsv.su.se>
2025-05-06 11:35:50 +0200

    Revert "fix id"
    This reverts commit 93ac6939ce34b8d6a527b7eaac99492681464d16.

--- src/main/java/com/pvt/demo/repository/TeamRepository.java
+++ src/main/java/com/pvt/demo/repository/TeamRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
-    List<Team> findByEventInstanceId(Long eventinstanceId);
+    List<Team> findByEventInstanceID(Long eventinstanceId);
 }
