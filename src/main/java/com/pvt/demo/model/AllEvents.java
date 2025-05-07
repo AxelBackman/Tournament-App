fb4623a1f479d1ca92936ae2fec6c5e5340ade8c
axba3072 <axba3072@student.su.se>
2025-05-07 14:02:11 +0200

    grejer

--- src/main/java/com/pvt/demo/model/AllEvents.java
+++ src/main/java/com/pvt/demo/model/AllEvents.java
@@ -1,8 +1,21 @@
 package com.pvt.demo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AllEvents {
 
-    
+
+    // Koppla mot de klassernas listor
+    List<RecurringEvent> recurringEvents = new ArrayList<>();
+    List<EventInstance> eventInstances = new ArrayList<>();
+    List<OneTimeEvent> oneTimeEvents = new ArrayList<>();
+
+
+    public AllEvents(){}
+
+
+
 
     
     
