6906cbf378fe09da8ab38fd77abd1a2151686ddb
mige4713 <mige4713@student.su.se>
2025-05-08 13:27:19 +0200

    RecurringEventController skapad

--- src/main/java/com/pvt/demo/controller/RecurringEventController.java
+++ src/main/java/com/pvt/demo/controller/RecurringEventController.java
@@ -7,11 +7,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pvt.demo.model.EventInstance;
 import com.pvt.demo.model.RecurringEvent;
-import com.pvt.demo.repository.EventInstanceRepository;
 import com.pvt.demo.repository.RecurringEventRepository;
-import com.pvt.demo.services.EventInstanceService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,26 +17,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
-@RequestMapping("/events")
+@RequestMapping("/recurringevents")
 @CrossOrigin()
-public class EventController {
-    @Autowired
-    private EventInstanceRepository eventInstanceRepository;
+public class RecurringEventController {
     
     @Autowired
     private RecurringEventRepository recurringEventRepository;
-    
-    @Autowired
-    private EventInstanceService eventInstanceService;
 
     @GetMapping("/allrecurring")
     public List<RecurringEvent> getAllRecurringEvents() {
         return recurringEventRepository.findAll();
-    }
-
-    @GetMapping("/allinstances")
-    public List<EventInstance> getAllEventInstances() {
-        return eventInstanceRepository.findAll();
     }
     
     @PostMapping("/addrecurring/{name}/{description}")
@@ -51,12 +38,6 @@ public class EventController {
         return "Recurring event added: " + event.getName();
     }
 
-    @PostMapping("/addinstance/{parentId}")
-    public String addEventInstance(@PathVariable Long parentId) {
-        EventInstance instance = eventInstanceService.addInstance(parentId);;
-        return "Event instance added of parent event: " + instance.getParentEvent().getName();
-    }
-
     @DeleteMapping("/deleterecurring/{id}")
     public String deleteRecurringEvent(@PathVariable Long id) {
         RecurringEvent event = recurringEventRepository.findById(id).orElse(null);
@@ -65,16 +46,6 @@ public class EventController {
         }
         recurringEventRepository.delete(event);
         return "Recurring event deleted: " + event.getName();
-    }
-
-    @DeleteMapping("/deleteinstance/{id}")
-    public String deleteEventInstance(@PathVariable Long id) {
-        EventInstance instance = eventInstanceRepository.findById(id).orElse(null);
-        if (instance == null) {
-            return "Event instance not found";
-        }
-        eventInstanceRepository.delete(instance);
-        return "Event instance deleted: " + instance.getParentEvent().getName();
     }
 
     @DeleteMapping("/deletallrecurring")
