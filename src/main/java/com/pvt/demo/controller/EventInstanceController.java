db3906378a346d1bd0f47f93f46f0bdec4bda96e
mige4713 <mige4713@student.su.se>
2025-05-08 13:23:17 +0200

    minor fix i eventinstancecontroller

--- src/main/java/com/pvt/demo/controller/EventInstanceController.java
+++ src/main/java/com/pvt/demo/controller/EventInstanceController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("eventinstances")
+@RequestMapping("/eventinstances")
 @CrossOrigin() 
 public class EventInstanceController {
 
