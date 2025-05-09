35c684ad42b8292459d5bcf2d98a335a98b556b1
Simon Kodu <siko0533@dsv.su.se>
2025-05-09 11:12:15 +0200

    Revert "oauth 2"
    This reverts commit 0b5ea6fc5daf2a83e83c1f83171e35b33fe0111d.

---
+++ src/main/java/com/pvt/demo/model/SpringBootOauth2GoogleApplication.java
@@ -0,0 +1,14 @@
+package com.pvt.demo.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2GoogleApplication {
+
+    public static void main(String[] args) {
+        // Start the Spring Boot application
+        SpringApplication.run(SpringBootOauth2GoogleApplication.class, args);
+    }
+
+}
