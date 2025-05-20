ad1c47cd5c2c8a060a34509e9ddc62462453bf81
mige4713 <mige4713@student.su.se>
2025-05-20 11:44:44 +0200

    Fler ändringar av TournamentController

--- src/main/java/com/pvt/demo/controller/TournamentController.java
+++ src/main/java/com/pvt/demo/controller/TournamentController.java
@@ -4,14 +4,20 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.pvt.demo.model.Tournament;
 import com.pvt.demo.repository.TournamentRepository;
 
+@RestController
+@RequestMapping("/tournaments")
+@CrossOrigin() 
 public class TournamentController {
 
     @Autowired
