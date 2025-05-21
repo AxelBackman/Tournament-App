cf41208286da4a03a14d10c320b8a9364fef5a97
axba3072 <axba3072@student.su.se>
2025-05-21 14:43:32 +0200

    Fix

--- src/main/java/com/pvt/demo/repository/EventInstanceRepository.java
+++ src/main/java/com/pvt/demo/repository/EventInstanceRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.pvt.demo.model.EventInstance;
 
 public interface EventInstanceRepository extends JpaRepository<EventInstance, Long> {
-    List<EventInstance> findByStartDateAndTournamentCreatedFalse(LocalDate startDate);
+    List<EventInstance> findByStartDateAndTournamentCreatedFalse(LocalDate startTime);
 }
