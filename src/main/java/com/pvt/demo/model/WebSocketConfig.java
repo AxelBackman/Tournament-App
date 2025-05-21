eed4a8eb858fe2eea95e729e0b768ad1b296dfba
mige4713 <mige4713@student.su.se>
2025-05-21 13:17:35 +0200

    La till ren Websocket-endpoint

--- src/main/java/com/pvt/demo/model/WebSocketConfig.java
+++ src/main/java/com/pvt/demo/model/WebSocketConfig.java
@@ -18,7 +18,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws") // WebSocket-URL
+        registry.addEndpoint("/ws") // Websocket
+                .setAllowedOriginPatterns("*");
+
+        registry.addEndpoint("/ws") //  SockJS
                 .setAllowedOriginPatterns("*")
                 .withSockJS(); // Fallback
     }
