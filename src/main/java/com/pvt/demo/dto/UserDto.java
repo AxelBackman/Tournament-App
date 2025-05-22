6ddcf84aa86c104816eda6511fbe742bb00f0004
mige4713 <mige4713@student.su.se>
2025-05-22 13:25:38 +0200

    Pattern-validering för username

--- src/main/java/com/pvt/demo/dto/UserDto.java
+++ src/main/java/com/pvt/demo/dto/UserDto.java
@@ -1,10 +1,12 @@
 package com.pvt.demo.dto;
 
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class UserDto {
     
     @Size(min = 2, max = 20, message = "Username must be min 2 and max 20 characters long")
+    @Pattern(regexp = "^[a-zA-Z0-9_åäöÅÄÖ\\- ]+$", message = "Username contains invalid characters")
     public String name;
     
     public String email;
