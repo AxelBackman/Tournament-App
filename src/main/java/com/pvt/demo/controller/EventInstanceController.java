c250b81a88636e83ab8b850927992c49e2ad604a
mige4713 <mige4713@student.su.se>
2025-05-22 10:55:15 +0200

    REMOVE YELLOW

--- src/main/java/com/pvt/demo/controller/EventInstanceController.java
+++ src/main/java/com/pvt/demo/controller/EventInstanceController.java
@@ -20,7 +20,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
