f6154087f1849fa7f01a6a261709511b2a3df717
Simon Kodu <siko0533@dsv.su.se>
2025-05-22 11:59:34 +0200

    Ta bort user när den är i team?

--- src/main/java/com/pvt/demo/controller/UserController.java
+++ src/main/java/com/pvt/demo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.pvt.demo.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pvt.demo.model.Organisation;
+import com.pvt.demo.model.Team;
 import com.pvt.demo.model.User;
 import com.pvt.demo.repository.OrganisationRepository;
+import com.pvt.demo.repository.TeamRepository;
 import com.pvt.demo.repository.UserRepository;
 import com.pvt.demo.dto.UserDto;
 
@@ -23,14 +26,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin() 
+@CrossOrigin()
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private OrganisationRepository organisationRepository;
-    
+
+    @Autowired
+    private TeamRepository teamRepository;
+
     @GetMapping
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
@@ -40,7 +46,7 @@ public class UserController {
     public String addNewUser(@RequestBody UserDto userDto) {
         // Hämta organisationen baserat på ID
         Organisation organisation = organisationRepository.findById(userDto.organisationId).orElse(null);
-        
+
         if (organisation != null) {
             User user = new User(userDto.name, userDto.email, organisation, userDto.isAdmin); // Skapa användare med organisation
             userRepository.save(user);
@@ -78,23 +84,22 @@ public class UserController {
 
         User user = userOpt.get();
 
-        // Hämta alla team där användaren är medlem
-        // List<Team> teams = teamRepository.findByMembers_Id(id);
+        List<Team> teams = teamRepository.findByMembers_Id(id);
 
 
-        // for (Team team : teams) {
-        //     team.getMembers().remove(user); 
-        //     if (team.getMembers().isEmpty()) {
-        //         teamRepository.delete(team); 
-        //     } else {
-        //         teamRepository.save(team); // Save team without the user
-        //     }
-        // }
+        for (Team team : teams) {
+            team.getMembers().remove(user);
+            if (team.getMembers().isEmpty()) {
+                teamRepository.delete(team);
+            } else {
+                teamRepository.save(team); // Save team without the user
+            }
+        }
         userRepository.delete(user);
         return "User with ID " + id + " deleted successfully";
     }
 
-    
+
 
 
     @DeleteMapping("/deleteall")
--- src/main/java/com/pvt/demo/model/Tournament.java
+++ src/main/java/com/pvt/demo/model/Tournament.java
@@ -50,6 +50,9 @@ public class Tournament {
         if (eventInstance != null) {
             eventInstance.setTournament(this);
         }
+
+        this.setTeams();
+        this.generateBracket();
     }
 
     public Tournament(EventInstance eventInstance, int teamSize, String grejer){ // samma konstruktor som ovan men särskiljer sig för mickes chat tjofräs
