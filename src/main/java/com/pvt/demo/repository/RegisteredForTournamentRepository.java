942ea4841fcfaa50fd9f307071f0cf00e67f02aa
mige4713 <mige4713@student.su.se>
2025-05-27 11:34:14 +0200

    Repo för RFT

---
+++ src/main/java/com/pvt/demo/repository/RegisteredForTournamentRepository.java
@@ -0,0 +1,13 @@
+package com.pvt.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pvt.demo.model.RegisteredForTournament;
+
+public interface RegisteredForTournamentRepository extends JpaRepository<RegisteredForTournament, Long> {
+    List<RegisteredForTournament> findByTournamentId(Long tournamentId);
+    RegisteredForTournament findByUserIdAndTournamentId(Long userId, Long tournamentId);
+    long countByTournamentId(Long tournamentId);
+}
